801bc0f9aaccbd06d2bf24089b14be8c8686a65e
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2022-08-04 21:57:33 +0530

    Update debug.py

--- FallenRobot/modules/debug.py
+++ FallenRobot/modules/debug.py
@@ -66,6 +66,6 @@ dispatcher.add_handler(LOG_HANDLER)
 DEBUG_HANDLER = CommandHandler("debug", debug)
 dispatcher.add_handler(DEBUG_HANDLER)
 
-__mod_name__ = "DEBUG"
+__mod_name__ = "𝙳𝙴𝙱𝚄𝙶"
 __command_list__ = ["debug"]
 __handlers__ = [DEBUG_HANDLER]
