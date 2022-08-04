d12d7264ed8445c3eb6228cf0f2a48771d76a750
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2022-08-04 21:41:08 +0530

    Update backups.py

--- FallenRobot/modules/backups.py
+++ FallenRobot/modules/backups.py
@@ -367,7 +367,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Bᴀᴄᴋᴜᴘ"
+__mod_name__ = "𝙱𝙰𝙲𝙺-𝚄𝙿"
 
 __help__ = """
 *Only for group owner:*
